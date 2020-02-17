f1c17db1a18f21ac76cb8e8cca24606159e3a744
truxai <premteastallstudio@gmail.com>
2020-02-17 12:05:16 +0530

    all picup length change

--- controller/dustbin.js
+++ controller/dustbin.js
@@ -402,7 +402,7 @@ const googleMapsClient = require('@google/maps').createClient({
                         db.query(sqlquery, function (error, todaypicupCount) {
 
                             if (error) throw error;
-                            var sqlquery ="SELECT assign_group_vehicle.id FROM `dustbins` INNER JOIN warehouses on warehouses.id=dustbins.warehouse_id INNER JOIN assign_group_vehicle on assign_group_vehicle.did=dustbins.id INNER join vehicles on vehicles.id=assign_group_vehicle.vid INNER JOIN mapping_vehicle_drivers on assign_group_vehicle.vid=mapping_vehicle_drivers.vehicle_id INNER JOIN drivers on drivers.id=mapping_vehicle_drivers.driver_id WHERE dustbins.id in(select did from assign_group_vehicle) and assign_group_vehicle.status=0";
+                            var sqlquery ="SELECT count(*) as allpicup FROM `dustbins` INNER JOIN warehouses on warehouses.id=dustbins.warehouse_id INNER JOIN assign_group_vehicle on assign_group_vehicle.did=dustbins.id INNER join vehicles on vehicles.id=assign_group_vehicle.vid INNER JOIN mapping_vehicle_drivers on assign_group_vehicle.vid=mapping_vehicle_drivers.vehicle_id INNER JOIN drivers on drivers.id=mapping_vehicle_drivers.driver_id WHERE dustbins.id in(select did from assign_group_vehicle) and assign_group_vehicle.status=0";
                             db.query(sqlquery, function (error, allCount) {
                                 if (error) throw error;
 
