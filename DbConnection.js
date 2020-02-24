90d57ee793342bbcf90c835faf149106fc935e05
truxai <premteastallstudio@gmail.com>
2020-02-24 11:42:16 +0530

    last new db

--- DbConnection.js
+++ DbConnection.js
@@ -11,8 +11,8 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-// con.end(function(err) {
-//   if (err) throw err;
-//   console.log("Disconnected!");
-// });
+con.end(function(err) {
+  if (err) throw err;
+  console.log("Disconnected!");
+});
 module.exports = con;
